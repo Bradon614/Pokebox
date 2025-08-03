b2bf4da946a9bf43b7cd36c8b45d1bf5bd6d1696
Koloina01 <kolomaherison@gmail.com>
2025-08-04 00:38:30 +0300

    chore: add tailwind.config.js with custom rotate y-180 utility

---
+++ Pokebox/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      rotate: {
+        'y-180': '180deg',
+      },
+    },
+  },
+  plugins: [],
+}
